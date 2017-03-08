58d49d0b7be7a3f954d7b9d30017f7dedacebed3
2005794 <2005794@AL-105-29.student.acalanes.k12.ca.us>
2017-03-08 11:54:59 -0800

    Activity 2 completed

--- src/Activity02/Deck.java
+++ src/Activity02/Deck.java
@@ -1,3 +1,5 @@
+package Activity02;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -31,6 +33,13 @@ public class Deck {
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+                cards = new ArrayList<Card>();
+                for(int i = 0; i < ranks.length; i++) {
+                        Card temp = new Card(ranks[i], suits[i], values[i]);
+                        cards.add(temp);
+                        size++;
+                    
+                }
 	}
 
 
@@ -40,6 +49,12 @@ public class Deck {
 	 */
 	public boolean isEmpty() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+                if(size == 0) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
 	}
 
 	/**
@@ -48,6 +63,7 @@ public class Deck {
 	 */
 	public int size() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+                return size;
 	}
 
 	/**
@@ -65,6 +81,15 @@ public class Deck {
 	 */
 	public Card deal() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+                if(size == 0) {
+                    return null;
+                }
+                else {
+                    Card temp = cards.get(size);
+                    size--;
+                    return temp;
+                    
+                }
 	}
 
 	/**
